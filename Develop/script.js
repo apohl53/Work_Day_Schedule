7102ed7180bc1328ab0a743847fee4d9594495d9
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-09-22 22:47:14 -0400

    tested and fixed the color coding

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,36 @@
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
 // Stops JavaScript from loading until HTML and CSS are done.
 $(document).ready(function () {
-  var displayTime = document.querySelector("#currentDay");
-  var currentTime = dayjs().format("dddd, MMMM D, YYYY, h:mm:ss a");
-  // THEN the current day is displayed at the top of the calendar
-  displayTime.textContent = currentTime;
+  var currentTime = dayjs();
 
+  var formattedTime = currentTime.format("dddd, MMMM D, YYYY, h:mm:ss a");
+
+  // THEN the current day is displayed at the top of the calendar
+  var displayTime = document.querySelector("#currentDay");
+  displayTime.textContent = formattedTime;
+
+  // THEN the text for that event is saved in local storage
   $(".saveBtn").on("click", function () {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    // Save text in local storage
     localStorage.setItem(time, text);
   });
 
-  // THEN each time block is color-coded to indicate whether it is in the past, present, or future
   function hourTracker() {
-    var currentHour = dayjs().hour();
+    var currentHour = currentTime.hour();
 
     $(".time-block").each(function () {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
+      // THEN each time block is color-coded to indicate whether it is in the past, present, or future
       if (currentHour > blockHour) {
         $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
       } else if (currentHour === blockHour) {
         $(this).removeClass("past");
         $(this).addClass("present");
+        $(this).removeClass("future");
       } else {
         $(this).removeClass("past");
         $(this).removeClass("present");
@@ -54,9 +42,6 @@ $(document).ready(function () {
   hourTracker();
 
   // THEN the saved events persist
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
   function savedEvents() {
     $(".time-block").each(function () {
       var blockHour = $(this).attr("id");
